0fbaa85455ff999a36b96814e4282337159d5cf5
William Fish <william.michael.fish@gmail.com>
2024-07-05 12:04:51 +0100

    GL-732: Stream text with psql

--- restore.sh
+++ restore.sh
@@ -39,12 +39,8 @@ BACKUP_FILE="tariff-merged-production.sql.gz"
 
 curl -o- "https://tariff:$BASIC_AUTH_PASSWORD@dumps.trade-tariff.service.gov.uk/$BACKUP_FILE" | \
   gzip -d | \
-  pg_restore -h "$POSTGRES_HOST" \
-  -U "$POSTGRES_USER"            \
-  -d "$POSTGRES_DATABASE"        \
-  --no-acl                       \
-  --no-owner                     \
-  --clean                        \
-  --verbose                      \
+  psql -h "$POSTGRES_HOST"         \
+  -U "$POSTGRES_USER"              \
+  -d "$POSTGRES_DATABASE"
 
 echo "SQL backup restored successfully" && exit 0
