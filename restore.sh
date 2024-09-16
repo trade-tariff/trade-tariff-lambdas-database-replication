ed51f328794e9befdcbf35415ba49cfc3ba747c6
William Fish <william.michael.fish@gmail.com>
2024-09-16 15:14:46 +0100

    BAU: Change desired counts

--- restore.sh
+++ restore.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-set -eo
+[[ -n $DEBUG ]] && set -o xtrace
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o noclobber
 
 if [ "$ENVIRONMENT" = "" ]; then
   echo "You need to set the ENVIRONMENT environment variable."
@@ -35,29 +39,46 @@ fi
 export PGPASSWORD="$POSTGRES_PASSWORD" # env var needed for psql
 BACKEND_SERVICES="backend-admin-uk backend-admin-xi backend-uk backend-xi worker-uk worker-xi"
 BACKUP_FILE="tariff-merged-production.sql.gz"
+CLUSTER_NAME="trade-tariff-cluster-$ENVIRONMENT"
 
-stop_connected_tasks() {
-  for service in BACKEND_SERVICES; do
-    local tasks
-    tasks=$(aws ecs list-tasks --cluster trade-tariff-cluster-$ENVIRONMENT --service-name $service | jq -r '.taskArns[]')
-    for task in $tasks; do
-      aws ecs stop-task --cluster trade-tariff-cluster-$ENVIRONMENT --task $task
-    done
+declare -A SERVICE_COUNTS
+
+get_desired_count_for_service() {
+    local service=$1
+
+    aws ecs describe-services --cluster trade-tariff-cluster-$ENVIRONMENT \
+        --services $service \
+        --query 'services[0].desiredCount' \
+        --output text
+}
+
+set_desired_count_for_service_to() {
+    local service=$1
+    local desired_count=$2
+
+    aws ecs update-service --cluster trade-tariff-cluster-$ENVIRONMENT \
+        --service $service \
+        --desired-count $desired_count > /dev/null
+}
+
+stop_services() {
+  for service in $BACKEND_SERVICES; do
+      SERVICE_COUNTS[$service]=$(get_desired_count_for_service $service)
+
+      set_desired_count_for_service_to $service 0
+  done
+
+  sleep 20 # Give the services time to stop
+}
+
+start_services() {
+  for service in $BACKEND_SERVICES; do
+      set_desired_count_for_service_to $service ${SERVICE_COUNTS[$service]}
   done
 }
 
-start_connected_tasks() {
-  for service in BACKEND_SERVICES; do
-    local tasks
-    tasks=$(aws ecs list-tasks --cluster trade-tariff-cluster-$ENVIRONMENT --service-name $service | jq -r '.taskArns[]')
-    for task in $tasks; do
-      aws ecs start-task --cluster trade-tariff-cluster-$ENVIRONMENT --task $task
-    done
-  done
-}
-
-echo "Stopping connected tasks"
-stop_connected_tasks
+echo "Stopping services"
+stop_services
 
 curl -o- "https://tariff:$BASIC_AUTH_PASSWORD@dumps.trade-tariff.service.gov.uk/$BACKUP_FILE" | \
   gzip -d | \
@@ -73,5 +94,5 @@ cat after_restore.sql | psql -h "$POSTGRES_HOST"         \
 
 echo "Applied after restore SQL script"
 
-echo "Starting connected tasks"
+echo "Starting services"
 start_connected_tasks
